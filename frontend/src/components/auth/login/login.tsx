e53581b8942b881d796544b2a1fdb5befe746c48
feodor <fedor15zil@gmail.com>
2023-03-05 20:17:48 +0300

    auth forms ui

--- frontend/src/components/auth/login/login.tsx
+++ frontend/src/components/auth/login/login.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import { Button, TextField, Typography } from '@mui/material';
+import React, { Fragment } from 'react';
 
-const LoginPage = () => {
+const LoginPage = (props: any) => {
+    const{setEmail,setPassword} =props
+
     return (
-        <div>
-            <h1>login page</h1>
-        </div>
+        <>
+            <Typography variant="h2" padding={3}>Sign In</Typography>
+            <TextField fullWidth={true} margin='normal' label="Email" variant="outlined" placeholder='Enter your email' 
+                onChange={(e) => {setEmail(e.target.value)}}/>
+            <TextField type="password" fullWidth={true} margin='normal' label="Password" variant="outlined" placeholder='Enter your password'
+                onChange={(e) => {setPassword(e.target.value)}}/>    
+            <Button type="submit" variant="contained" sx={{marginTop: 3}}>Sign In</Button>
+            <Typography variant="body1" padding={3}>No account?<span className="incitingText">Register</span></Typography>
+
+        </>
     );
 };
 
--- frontend/src/components/auth/register/register.tsx
+++ frontend/src/components/auth/register/register.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { Typography, TextField, Button } from '@mui/material';
+import React, { Fragment } from 'react';
 
 const RegisterPage = () => {
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <Fragment>
+            <Typography variant="h2" padding={3}>Register</Typography>
+            <TextField fullWidth={true} margin='normal' label="Email" variant="outlined" placeholder='Enter your email'/>
+            <TextField type="password" fullWidth={true} margin='normal' label="Password" variant="outlined" placeholder='Enter your password'/>    
+            <Button variant="contained" sx={{marginTop: 3}}>Register</Button>
+            <Typography variant="body1" padding={3}>Already have an account?<span className="incitingText">Sign In</span></Typography>
+
+        </Fragment>
     );
 };
 
